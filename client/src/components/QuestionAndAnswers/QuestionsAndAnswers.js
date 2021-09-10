c66dccbd68de53470582e45eb54dbb571607e4b1
Andy Fang <afang1094@gmail.com>
2021-09-09 17:20:25 -0700

    created QuestionsAndAnswers folder and file

---
+++ client/src/components/QuestionAndAnswers/QuestionsAndAnswers.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class QuestionsAndAnswers extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return(
+      <div>
+
+      </div>
+    )
+  }
+}
+
+extend default QuestionsAndAnswers;
