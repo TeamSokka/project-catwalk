73956ae763f864b027efce62451cc8d6c4fd8109
Derek Samms <derek.samms@gmail.com>
2021-09-09 19:02:41 -0700

    added basic code to create React Component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,7 +7,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>Hello World!</div>
+      <div>Hello, World!</div>
     )
   }
 }
--- client/src/components/product-details/ProductDetail.jsx
+++ client/src/components/product-details/ProductDetail.jsx
@@ -1,1 +1,16 @@
 //ProductDetail extends React.Component
+import React from 'react';
+
+class ProductDetail extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>Hello, World!</div>
+    )
+  }
+}
+
+export default ProductDetail;
