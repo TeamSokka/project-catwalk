a10e2e0fe8df2651e931993700dd33b595a47d71
John Fa <john6fa@gmail.com>
2021-09-09 17:23:55 -0700

    ratings and reviews js

---
+++ client/src/components/ratings&reviews/RatingsAndReviews.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class RatingsAndReviews extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+
+      </div>
+    )
+  }
+}
+
+export default RatingsAndReviews;
