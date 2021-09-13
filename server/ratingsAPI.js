dd55dc15b238a40be8adf78d0351418957f475b4
John Fa <john6fa@gmail.com>
2021-09-13 11:11:47 -0700

    ratings api http requests work

--- server/ratingsAPI.js
+++ server/ratingsAPI.js
@@ -1,40 +1,53 @@
 // https://app-hrsei-api.herokuapp.com/api/fec2/:CAMPUS_CODE/
-
+// http://example.com/page?parameter=value&also=another
 // axios
 // config
 const axios = require('axios');
 const config = require('../config.js');
 
-// Come back
 
 // GET reviews
-
-/* GET /reviews/
-Parameter	    Type	       Description
-page         	integer	     Selects the page of results to return. Default 1.
-count       	integer	     Specifies how many results per page to return. Default 5.
-sort	        text	       Changes the sort order of reviews to be based on "newest", "helpful", or "relevant"
-product_id  	integer	     Specifies the product for which to retrieve reviews.
-
-Response: Status: 200 OK
-*/
-
-
-
 // GET metadata
-/*
-GET /reviews/meta
-Parameter	    Type     	Description
-product_id	  integer	  Required ID of the product for which data should be returned
 
-Response: Status: 200 OK
-*/
+// https://app-hrsei-api.herokuapp.com/api/fec2/:CAMPUS_CODE/
+// http://example.com/page?parameter=value&also=another
+const getReviews = (body, callback) => {
+  axios.get(
+    `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/?product_id=${body.product_id}&page=${body.page}&count=${body.count}&sort=${body.sort}`, body, {
+    headers: {
+      Authorization: config.TOKEN,
+    }
+  })
+    .then((response) => {
+      console.log('getReviews API data: ' + response.data);
+      callback(null, response.data);
+    })
+    .catch((error) => {
+      console.log('Error getReviews API: ' + error);
+      callback(error, null);
+    })
+}
+
+const getMetaReviews = (body, callback) => {
+  axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/meta/?product_id=${body.product_id}`, body, {
+    headers: {
+      Authorization: config.TOKEN,
+    }
+  })
+    .then((response) => {
+      console.log('getMetaReviews API data: ' + response.data);
+      callback(null, response.data);
+    })
+    .catch((error) => {
+      console.log('Error getMetaReviews API: ' + error);
+      callback(error, null);
+    })
+}
 
 
 // POST reviews
 /*
 POST /reviews
-
 Response: Status: 201 CREATED
 */
 const postReviews = (body, callback) => {
@@ -43,30 +56,43 @@ const postReviews = (body, callback) => {
       Authorization: config.TOKEN,
     }
   })
-  .then((response) => {
-    console.log('postReviews API data: ' + response.data);
-    callback(null, response);
-  })
-  .catch((error) => {
-    console.log('Error postReviews API: ' + error);
-    callback(error, null);
-  })
+    .then((response) => {
+      console.log('postReviews API data: ' + response.data);
+      callback(null, response.data);
+    })
+    .catch((error) => {
+      console.log('Error postReviews API: ' + error);
+      callback(error, null);
+    })
 }
 
 
 // PUT reviews
 /*
 PUT /reviews/:review_id/helpful
-
 Response: Status: 204 NO CONTENT
 */
 
 /*
 PUT /reviews/:review_id/report
-
 Response: Status: 204 NO CONTENT
 */
 
+const putReviews = (body, callback) => {
+  axios.put(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfp/reviews/${body.review_id}/${body.type}`, {}, {
+    headers: {
+      Authorization: config.TOKEN,
+    }
+  })
+    .then((response) => {
+      console.log('putReviews API data: ' + response.data);
+      callback(null, response.data);
+    })
+    .catch((error) => {
+      console.log('Error putReviews API: ' + error);
+      callback(error, null);
+    })
+}
 
 
 /*
