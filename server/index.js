d75e0754dced411456fbc733b360afb3b7f5ab28
John Fa <john6fa@gmail.com>
2021-09-14 17:42:49 -0700

    merge with qa rioc

--- server/index.js
+++ server/index.js
@@ -15,7 +15,6 @@ app.use(express.static(path.join(__dirname, '..', 'client/dist')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
 // Products
 // Reviews
 // REVIEWS WIDGET
