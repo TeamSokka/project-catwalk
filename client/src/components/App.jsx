1c2bf762f8dc8be83de139d589a04d1b84979af8
John Fa <john6fa@gmail.com>
2021-09-18 10:00:28 -0700

    uncommented comp in app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -96,12 +96,12 @@ class App extends React.Component {
 
     return (
       <div>
-        {/* <ProductDetail
+        <ProductDetail
           productID={productID}
           productInfo={productInfo}
           styles={styles}
           selectedStyle={selectedStyle}
-        /> */}
+        />
 
 
         {
