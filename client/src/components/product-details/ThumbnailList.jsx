36a882c372de3731bcdfe357cfe4322e48b239f4
Derek Samms <derek.samms@gmail.com>
2021-09-18 14:56:21 -0700

    installed font-awesome

--- client/src/components/product-details/ThumbnailList.jsx
+++ client/src/components/product-details/ThumbnailList.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 
 var ThumbnailList = (props) => (
     <div id="thumbnail-list">
-      {props.topThumbnail > 0 && <i className="arrow up" onClick={props.handleUpArrowClick}>^</i>}
+      {props.topThumbnail > 0 && <i className="fas fa-chevron-circle-up" onClick={props.handleUpArrowClick}>^</i>}
       {props.photos.slice(props.topThumbnail, props.topThumbnail + 7).map((photo, index) => (
           <img className='thumbnail' key={index} data-index={index} src={photo.thumbnail_url} width="100" onClick={props.handleThumbnailClick}></img>
       ))}
-      {props.topThumbnail < props.photos.length - 7 && <i className="arrow down" onClick={props.handleDownArrowClick}>V</i>}
+      {props.topThumbnail < props.photos.length - 7 && <i class="fas fa-chevron-circle-down" onClick={props.handleDownArrowClick}></i>}
     </div>
 );
 
