df6b03aa7d76511611fb5ea7788b31a5005aa118
Stormi <stormikhashimoto@gmail.com>
2021-09-18 10:43:02 -0700

    merge related products to main

--- client/src/components/RelatedItems&OutfitCreation/RelatedItems.jsx
+++ client/src/components/RelatedItems&OutfitCreation/RelatedItems.jsx
@@ -27,3 +27,4 @@ export default RelatedItems;
 1.4.1.3.  Action button
 A button will appear on the top right corner of each product card.  This button will trigger different functionality depending on which list, Related Products or Your Outfit, the card appears within. The button will also display a different value depending on which list the card appears within.
 */
+
