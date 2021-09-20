3f99f60f5242a1064e44b507d1c9947e11ae5093
Andy Fang <afang1094@gmail.com>
2021-09-20 10:07:11 -0700

    commit before merge with main

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -111,15 +111,15 @@ class App extends React.Component {
 
     return (
       <div>
-         {/* <ProductDetail
+         <ProductDetail
           productID={productID}
           productInfo={productInfo}
           styles={styles}
           selectedStyle={selectedStyle}
           handleStyleSelect={this.handleStyleSelect.bind(this)}
-        /> */}
+        />
 
-        {/* {
+        {
           this.state.metaReady === true
           &&
           <RatingsAndReviews
@@ -127,20 +127,20 @@ class App extends React.Component {
             metaData={metaData}
             productInfo={productInfo}
           />
-        } */}
+        }
 
         <QuestionsAndAnswers
         productID={productID}
         productInfo={productInfo}
         />
 
-        {/* <RelatedItems
+        <RelatedItems
           productID={productID}
           productInfo={productInfo}
           relatedProducts={relatedProducts}
           selectedStyle={selectedStyle}
           getProductInfo={this.getProductInfo}
-        /> */}
+        />
       </div>
     )
   }
