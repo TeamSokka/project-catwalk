1d2f797717faecaee431b01aa84e1fd82a2f45b8
Stormi <stormikhashimoto@gmail.com>
2021-09-19 21:28:44 -0700

    Related product card display without action button functionality

---
+++ spec/ProductDetail.test 3.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import App from '../../client/src/components/App';
+const ProductDetail = require('../client/src/components/product-details/ProductDetail.jsx');
+
+describe('App', () => {
+  test('renders App component', () => {
+    render(<App />);
+  });
+});
+
+
+//Tests will go here.
+
+// test('it should do something', () => {
+//   expect(true).toBeTruthy();
+// });
+
+//Image Gallery
+
+
+//Product Information
+
+//Add to Cart section
+
+//Style Selector
