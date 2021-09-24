9c21d8f47044f4142bbcce826c73935a8e5f25ca
Stormi <stormikhashimoto@gmail.com>
2021-09-23 20:03:59 -0700

    related product card clickable to change products

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Suspense } from 'react';
 
-import Banner from './Banner.jsx';
+// import Banner from './Banner.jsx';
 import QuestionsAndAnswers from '../components/Q&A/QuestionsAndAnswers.jsx';
 import Cards from './RelatedItems&OutfitCreation/Cards';
 import RatingsAndReviews from './ratings&reviews/RatingsAndReviews';
@@ -18,7 +18,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productID: 40348, // example product id, change to num
+      productID: 40345,
       productInfo: {},
       relatedProducts: [],
       styles: [],
@@ -26,7 +26,6 @@ class App extends React.Component {
       metaData: {},
       metaReady: false,
     }
-    // this.fetchMeta = this.fetchMeta.bind(this);
   }
 
   componentDidMount() {
@@ -43,9 +42,7 @@ class App extends React.Component {
       productInfo: data
     })
   }
-  /*stormi: refactor function to take in id, callback. The callback is defaulted to setProductInfo.
-    This allows me to pass in a custom callback without changing the productInfo state
-  */
+
   getProductInfo = (id, callback = this.setProductInfo) => {
     axios.get(`/products/${id}`)
       .then(({ data }) => {
@@ -59,13 +56,10 @@ class App extends React.Component {
   getStyles() {
     axios.get(`/products/${this.state.productID}/styles`)
       .then((res) => {
-        // console.log('skus[0]:', Object.keys(res.data.results[0].skus)[0])
         this.setState({
           styles: res.data.results,
           selectedStyle: res.data.results[0],
-          // selectedSize: res.data.results[0].skus[Object.keys(res.data.results[0].skus)[0]]
         });
-        // console.log('state:', this.state)
       })
       .catch((err) => {
         console.error(err);
@@ -74,8 +68,6 @@ class App extends React.Component {
 
   handleStyleSelect(event) {
     event.preventDefault();
-    // console.log('event.target:', event.target);
-    // console.log('event.target.dataset.index:', event.target.dataset.index);
     this.setState({
       selectedStyle: this.state.styles[event.target.dataset.index]
     })
@@ -84,7 +76,6 @@ class App extends React.Component {
   getRelated = () => {
     axios.get(`/products/${this.state.productID}/related`)
       .then((res) => {
-        // console.log('related products recd:', res.data);
         this.setState({
           relatedProducts: res.data
         })
@@ -94,7 +85,18 @@ class App extends React.Component {
       });
   }
 
-  // Get meta reviews
+  handleProductChange = (id) => {
+    console.log('id: ', id);
+    this.getProductInfo(id);
+    this.getRelated(id);
+    this.fetchMeta();
+    this.getStyles();
+    this.setState({
+      ...this.state,
+      productID: id
+    });
+  }
+
   fetchMeta() {
     const { productID } = this.state;
     axios.get(`/reviews/meta/?product_id=${productID}`)
@@ -112,16 +114,13 @@ class App extends React.Component {
   render() {
     const { productID, productInfo, relatedProducts, styles, selectedStyle, metaData, metaReady } = this.state;
 
-    // console.log('app state// productInfo', productInfo);
-    // console.log('app state// relatedPro', relatedProducts);
-
     return (
       <div>
         {/* <Suspense fallback={<div>Loading...</div>}>
           <section>
           </section>
         </Suspense> */}
-        <Banner />
+        {/* <Banner /> */}
 
         <ProductDetail
           productID={productID}
@@ -137,6 +136,7 @@ class App extends React.Component {
         <Cards
           productInfo={productInfo}
           relatedProducts={relatedProducts}
+          handleProductChange={this.handleProductChange}
         />
 
         <QuestionsAndAnswers
--- client/src/components/RelatedItems&OutfitCreation/Related/RelatedProductCards.jsx
+++ client/src/components/RelatedItems&OutfitCreation/Related/RelatedProductCards.jsx
@@ -5,10 +5,19 @@ import { VscTriangleLeft, VscTriangleRight } from 'react-icons/vsc';
 import '../related-items.scss';
 
 const RelatedProductCards = (props) => {
-  const { productInfo, relatedProducts, handleComparison } = props;
+  const { productInfo, relatedProducts, handleComparison, handleProductChange } = props;
+
+  const handleSubmitNewProduct = (id) => {
+    // console.log('id: ', id);
+    handleProductChange(id);
+  }
 
   const products = relatedProducts.map((product, index) =>
-    <div className="product-card" key={index}>
+    <div
+      className="product-card"
+      key={index}
+      onClick={() => handleSubmitNewProduct(product.id)}
+    >
       <ProductPreviewImages
         productStyles={product.styles}
         id={index}
